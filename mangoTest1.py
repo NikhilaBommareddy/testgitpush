c5eaca41042376239f6841323e8129cc88057d34
Nikhila Bommareddy <nikhilaashokreddy@gmail.com>
2023-06-11 09:06:42 -0400

    This is my first github push

---
+++ mangoTest1.py
@@ -0,0 +1,15 @@
+import pymongo
+
+client=pymongo.MongoClient("mongodb+srv://nikhilabommareddy:NikhilaAshok@cluster0.lfzadum.mongodb.net/?retryWrites=true&w=majority")
+db= client.test
+print(db)
+
+d={
+     "name" : "Nikhila",
+      "surname" : "Bommareddy",
+     "email" : "nikhilaashokreddy@gmail.com"
+}
+
+db1=client['mangoTest1']
+coll= db1['test']
+coll.insert_one(d)
